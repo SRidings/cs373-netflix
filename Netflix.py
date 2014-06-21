c81300a8028bf68a02a2551578decf76fbdaa60d
Stephen Ridings <sridings777@aol.com>
2014-06-21 16:07:09 -0500

    Started to implement the prediction #1 algorithm and cache reader

--- Netflix.py
+++ Netflix.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-import math
+import math,json
+from pprint import pprint
+
+#Average of every movie rated
+glbl_mean=3.604289964420661
 
 def netflix_read(r):
     """
@@ -8,7 +12,18 @@ def netflix_read(r):
     """
     s=r.readline()[:-1]
     return s
-#       return netflix_predict(s)
+
+def readAvgMovieRating(movieID) :
+    """
+    read from cache of avg movie ratings
+    """
+    #with open("/u/mukund/cs373-netflix-tests/") as f:
+    with open("/u/sridings/cs373-netflix/json_ex.json") as f:
+        data=json.load(f)
+        pprint(data)
+        
+      
+    return 0
 
 def sqre_diff(a, p) :
     return (a - p) ** 2
@@ -16,7 +31,11 @@ def sqre_diff(a, p) :
 def netflix_write (s, w) :
     w.write(str(s) + "\n")
 
-def netflix_predict(cust) :
+def netflix_predict(movieID) :
+    """
+        Currently experimenting with implementation #1
+    """
+    readAvgeMovieRating(movieID)
     return 0
 
 def netflix_rate(r, w) :
@@ -38,3 +57,7 @@ def rmse (a, p) :
     v = sum(map(sqre_diff, a, p))
     return math.sqrt(v / s)
 
+def main() : 
+     readAvgMovieRating(0)
+
+main()
